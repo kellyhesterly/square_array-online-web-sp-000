7a67bbe149a07ea2a7ee0d1c76cad99fd26baf6f
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-01-28 19:28:36 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,6 @@
 def square_array(numbers)
   new_square_array = []
-  numbers.each do |number|
-    new_square_array << number ** 2
+  numbers.each {|number| new_square_array << number ** 2}
     new_square_array
   end
 end
