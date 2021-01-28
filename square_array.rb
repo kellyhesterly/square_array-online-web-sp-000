a8213ffefa02bc4455d2d6dc447beeb332ffd799
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-01-28 19:33:02 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
-# def square_array(numbers)
-#   new_square_array = []
-#   numbers.each {|num| new_square_array << num ** 2}
-#   new_square_array
-#   end
+def square_array(numbers)
+  new_square_array = []
+  numbers.each {|num| new_square_array << num ** 2}
+  new_square_array
+  end
 
-def square_array(array)
-  array.collect {|num| num ** 2}
-end
+# def square_array(array)
+#   array.collect {|num| num ** 2}
+# end
